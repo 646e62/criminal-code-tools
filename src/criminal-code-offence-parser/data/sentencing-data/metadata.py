58b88f223ec207e512b5b11eda070f1b5bfcabd5
646e62 <444e42@protonmail.com>
2024-12-17 07:24:10 -0600

    Updated ss 246 and 264.1 to properly reflect subsections, added more cases to BCPC

--- src/criminal-code-offence-parser/data/sentencing-data/metadata.py
+++ src/criminal-code-offence-parser/data/sentencing-data/metadata.py
@@ -6,12 +6,13 @@ UNAVAILABLE_COURT_DECISIONS = [
     "2024canlii98004", "2024canlii93152", "2024canlii93149",
     "2024canlii92654", "2024canlii90910", "2024canlii77399",
     "2024canlii27145", "2024canlii344", "2024canlii343", "2024abcj58",
-    
+    "2024canlii122870",
+
 ]
 
 LATEST_CASE_CHECKED = {
     "bcpc": "2024bcpc230",
-    "abcj": "2024abcj255",
+    "abcj": "2024abcj257",
     "skpc": "2024skpc40",
     "mbpc": "2024mbpc96",
     "oncj": "2024oncj643",
@@ -19,8 +20,24 @@ LATEST_CASE_CHECKED = {
     "nbpc": "2024nbpc8",
     "nspc": "2024nspc50",
     "pepc": "2009canlii101169",
-    "nlpc": "2024canlii121604",
+    "nlpc": "2024canlii123250",
     "yktc": "2024yktc42",
     "nttc": "2024nwttc7",
     "nucj": "2024nucj34",
+}
+
+# Stores cases where the database entry doesn't completely match the decision's
+# logic. The key is the case number, and the value is a tuple where the first
+# item is the reason for the discrepancy and the second is a description of how
+# the discrepancy was resolved in the database entry.
+
+PROBLEM_CASES = {
+    "2024bcpc114": (
+        "uncalculated totality in the original decision", 
+        "manually adjusted for totality by running sentences concurrent"
+        ),
+    "2024bcpc82": (
+        "offence date not included, unclear if the court ordered probation",
+        "used the decision date as the offence date, assumed no probation"
+    )
 }
